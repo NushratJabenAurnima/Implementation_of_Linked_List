faee59c6cf3d57975a8e0d40374f351e53e5821e
Nushrat Jaben Aurnima <134672494+NushratJabenAurnima@users.noreply.github.com>
2024-01-02 15:44:29 +0600

    Update SLL(ALL).c

--- SLL(ALL).c
+++ SLL(ALL).c
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+
 #include<stdio.h>
 struct node{
   int val;
